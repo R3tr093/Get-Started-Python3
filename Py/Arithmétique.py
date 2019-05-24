13b815474590ac436a6d528577618622ff711e06
R3tr093 <mossiat.jeoffrey@outlook.com>
2019-05-24 14:41:08 +0200

    Ajout de l'Arithmétique

---
+++ Py/Arithmétique.py
@@ -0,0 +1,44 @@
+
+print ("Ce programme traite des bases des opérations arithmétiques ")
+
+
+
+print (" \n Une addition s'écrit comme ceci : 5 + 3 ")
+
+a = 5 + 3
+
+b = 2 - 9
+
+c = 20 / 3
+
+d = 10 // 2
+
+e = (7 + 3) * 4
+
+
+
+print ( "\n Résultat = " + str (a)) # addition
+
+print ("\n Une soustraction s'écrit comme ceci : 2 - 9 ") 
+
+print (" \n Résultat = " + str(b)) # Soustraction
+
+print ( " \n En python, la hiérarchie des opérations est respectée, voici le résultat du calcul suivant (7 + 3 ) * 4 ")
+
+print ( " \n Résultat = "  + str(e)) # Hiérarchie des opérations ? > TRUE
+
+print (" \n La division s'écrit comme ceci : 20 / 3 ")
+
+print (" \n Résultat = " + str (e)) # Division
+
+
+
+print (" \n Donc en résumé, les opérateurs arithmétiques de base sont : + - * / " )
+
+print (" \n Notons tout de même déjà une particularité.")
+
+
+print ("\n Pour une division entière depuis Python 3, on utilise //, par exemple : 10//2 ")
+
+print ("\n Résultat = " + str (10//2)) # Division entière
+
