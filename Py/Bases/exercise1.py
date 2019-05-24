1b4c72da62895a672e143d58dab309a7ba83119a
R3tr093 <mossiat.jeoffrey@outlook.com>
2019-05-24 16:16:05 +0200

    Ajout des exercices

---
+++ Py/Bases/exercise1.py
@@ -0,0 +1,43 @@
+print (" Exercices relatifs au base vu précédemment ")
+
+
+print (" \n Exercice 1 : ")
+
+print (" \n Décrivez le plus clairement possible et complètement possible ce qui se passe à chacune des trois lignes d'instructions ci-dessous. ")
+
+print ( " \n Largeur = 20 " )
+
+print ( " \n Hauteur = 5 * 9.3 ")
+
+print ( " \n A = Largeur * Hauteur ")
+
+
+print ( " \n On déclare une variable Largeur qui prend pour valeur un nombre entier : 20 " )
+
+print (" \n On déclare une variable Hauteur qui prend pour valeur un nombre entier équivalent 5 multiplié par 9,3 ")
+
+print (" \n On déclare une variable A qui prend pour valeur le résultat de la mutliplication de notre variable Largeur avec la variable Hauteur ")
+
+
+
+print (" \n Exercice 2 : " )
+
+
+print (" \n Assignez les valeurs respectives 3, 5, 7 à trois variables a,b,c. ")
+
+print (" \n Ensuite, effectuez l'opération a-b //c Intrerprétez ensuite le résultat obtenu ")
+
+print ("\n a, b, c = 3,5,7 \n" )
+
+a,b,c = 3,5,7 
+
+
+print ("\n")
+
+print (a-b//c)
+
+
+
+
+
+
