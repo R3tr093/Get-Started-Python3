e3e6c78e04272672842e58bd7a2d4219796b62ff
Mossiat <mossiat.jeoffrey.com>
2019-05-26 23:38:35 +0200

    Ajout de fluc.py

---
+++ Py/Bases/flux.py
@@ -0,0 +1,24 @@
+# Voici un exemple d'une condition en Python
+
+
+a = -5
+
+# On écrit if suivi de la condition et on ferme avec :
+
+# Ensuite on passe à la ligne, on décale de 4 espaces, et on écris les instructions.
+
+if a > 0 : # Si a est supérieur à 0
+    print (" a est positif. " )
+
+
+if a < 0 : # si est inférieur à O.
+    print (" a est négatif.")
+
+
+# Si on veut ajouter une close else, elle dois ce situer au même niveau d'indentation, suivi d'un :
+
+else :
+    print (" a est nul, a vaut 0")
+
+
+
